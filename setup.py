183c9225f624c1c505c64dea88939edb3712f768
Chris Gill <chris.gill@autodesk.com>
2018-12-12 02:40:16 -0800

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,68 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'plaster_pastedeploy',
+    'pyramid',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'waitress',
+    'alembic',
+    'pyramid_retry',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'wtforms',
+    'webhelpers2',
+    'passlib',
+    'bigchaindb-driver'
+]
+
+tests_require = [
+    'WebTest >= 1.3.1',  # py3 compat
+    'pytest >= 3.7.4',
+    'pytest-cov',
+]
+
+setup(
+    name='bga',
+    version='0.0',
+    description='bga',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+    ],
+    author='',
+    author_email='',
+    url='',
+    keywords='web pyramid pylons',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    extras_require={
+        'testing': tests_require,
+    },
+    install_requires=requires,
+    entry_points={
+        'paste.app_factory': [
+            'main = bga:main',
+        ],
+        'console_scripts': [
+            'initialize_bga_db=bga.scripts.initialize_db:main',
+        ],
+    },
+    dependency_links=[
+        "https://github.com/KarmaPoliceT2/cryptoconditions/tarball/master#egg=cryptoconditions-0.8.0"
+    ],
+)
